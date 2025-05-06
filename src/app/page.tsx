9699bbda309c8566a697e577ae2d3748f5b849c9
karim-dev-10 <karim.iscod@gmail.com>
2025-05-06 02:12:51 +0200

    Deplace HOMEPAGE.tsx for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Footer from './components/Footer';
 import Navbar from './components/Navbar'
 import SocialBar from './components/SocialBar';
-import Homepage from './pages/Homepage';
+import Homepage from './components/Homepage';
  
 export default function Home() {
   return (
