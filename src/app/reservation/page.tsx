c03bae6c9364791a26ff6a73067dfdcdd66dc419
karim-dev-10 <karim.iscod@gmail.com>
2025-05-10 00:53:12 +0200

    Calendar french

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -7,7 +7,7 @@ export default function ReservationPage() {
       <Navbar />
       <main className="bg-pink-50">
         <iframe
-          src="https://cal.com/wafou/prestation"
+          src="https://cal.com/wafou/prestation?lang=fr"
           className="w-full min-h-screen h-full border-0"
         ></iframe>
       </main>
